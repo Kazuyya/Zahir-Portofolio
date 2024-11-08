36d48a33d54761b16b89424d91f9ce31c4dce313
Kazuyya <zahirraihanhakim.01@gmail.com>
2024-11-08 08:47:45 +0700

    Update 3

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -15,7 +15,7 @@ const works = [
     imgSrc: '/images/p1.png',
     title: 'Moti Motion',
     tags: ['UNITY', 'Programmer'],
-    projectLink: '../Html/project-detail.html'
+    projectLink: '../Html/pd1.html'
   },
   {
     imgSrc: '/images/project-2.jpg',
